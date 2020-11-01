66a9b331973f09b77e71c7acdb1d734e69574b19
Taylor Yang <rainbowww5@gmail.com>
2020-10-31 22:19:51 -0700

    feature to show answer

--- src/App.js
+++ src/App.js
@@ -2,68 +2,94 @@ import "./App.css";
 import questions from "./data/Apprentice_TandemFor400_Data";
 import React, { Component } from "react";
 import QuestionBox from "./components/questionBox";
+import ScoreBox from "./components/scoreBox";
 // import StartBox from "./components/startBox";
 
 class App extends Component {
   state = {
     isStartGame: false,
+    isAnswered: false,
     isEndGame: false,
     randomQuestions: null,
     currentQuestion: null,
     currentChoices: null,
+    currentAnswer: null,
     questionIndex: 0,
     score: 0,
   };
-
+  shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
   handleStartGame = () => {
     //Get 10 random questions from json
     const randomQuestions = questions
       .sort(() => 0.5 - Math.random())
       .slice(0, 10);
+
     this.setState({
       randomQuestions: randomQuestions,
       currentQuestion: randomQuestions[0],
-      currentChoices: [
+      currentChoices: this.shuffleArray([
         ...randomQuestions[0]["incorrect"],
         randomQuestions[0]["correct"],
-      ],
+      ]),
       isStartGame: true,
+      questionIndex: 0,
     });
-    console.log("Start game!", this.state.isStartGame);
   };
   handleGetNextQuestion = () => {
-    console.log(this.state.questionIndex);
     const randomQuestions = this.state.randomQuestions;
     const questionIndex = this.state.questionIndex + 1;
 
     if (questionIndex < 10) {
       this.setState({
         currentQuestion: randomQuestions[questionIndex],
-        currentChoices: [
+        currentChoices: this.shuffleArray([
           ...randomQuestions[questionIndex]["incorrect"],
           randomQuestions[questionIndex]["correct"],
-        ],
+        ]),
         questionIndex: questionIndex,
+        isAnswered: false,
       });
     } else {
       this.setState({
         isEndGame: true,
+        isStartGame: false,
       });
     }
+  };
+  handleRestart = () => {
+    this.setState({
+      isEndGame: false,
+    });
+  };
+  handleCheckAnswer = () => {
+    this.setState({ isAnswered: true });
   };
 
   render() {
     return (
       <React.Fragment>
-        <QuestionBox
-          currentQuestion={this.state.currentQuestion}
-          currentChoices={this.state.currentChoices}
-          // randomQuestions={this.state.randomQuestions}
-          isStartGame={this.state.isStartGame}
-          isEndGame={this.state.isEndGame}
-          onStartGame={this.handleStartGame}
-          onGetNextQuestion={this.handleGetNextQuestion}
-        />
+        {this.state.isEndGame ? (
+          <ScoreBox onRestart={this.handleRestart} />
+        ) : (
+          <QuestionBox
+            currentQuestion={this.state.currentQuestion}
+            currentChoices={this.state.currentChoices}
+            // randomQuestions={this.state.randomQuestions}
+
+            isAnswered={this.state.isAnswered}
+            isStartGame={this.state.isStartGame}
+            isEndGame={this.state.isEndGame}
+            onStartGame={this.handleStartGame}
+            onCheckAnswer={this.handleCheckAnswer}
+            onGetNextQuestion={this.handleGetNextQuestion}
+          />
+        )}
       </React.Fragment>
     );
   }
--- src/components/questionBox.jsx
+++ src/components/questionBox.jsx
@@ -3,53 +3,64 @@ import Jumbotron from "react-bootstrap/Jumbotron";
 import ListGroup from "react-bootstrap/ListGroup";
 
 class QuestionBox extends Component {
-  shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+  revealAnswer = (choices, choice) => {
+    if (this.props.isAnswered) {
+      if (choice === choices["correct"]) {
+        return "success";
+      } else {
+        return "danger";
+      }
+    } else {
+      return "secondary";
     }
-  }
+  };
 
   render() {
     let isStartGame = this.props.isStartGame;
-    console.log(this.props.currentQuestion);
+    let choices = this.props.currentChoices;
+
+    // const choices = this.shuffleArray(this.props.currentChoices);
     return (
-      <div>
-        <Jumbotron>
-          {isStartGame ? (
-            <div>
-              <h2>{this.props.currentQuestion["question"]}</h2>
+      <Jumbotron>
+        {isStartGame ? (
+          <div>
+            <h3>{this.props.currentQuestion["question"]}</h3>
+            <ListGroup>
+              {choices.map((choice) => (
+                <ListGroup.Item
+                  action
+                  variant={this.revealAnswer(
+                    this.props.currentQuestion,
+                    choice
+                  )}
+                  onClick={this.props.onCheckAnswer}
+                  key={this.props.currentChoices.indexOf(choice)}
+                >
+                  {choice}
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+            <br></br>
+            {this.props.isAnswered ? (
               <ListGroup>
-                {this.props.currentChoices.map((choice) => (
-                  <ListGroup.Item
-                    action
-                    className="outline-secondary"
-                    key={this.props.currentChoices.indexOf(choice)}
-                  >
-                    {choice}
-                  </ListGroup.Item>
-                ))}
+                <ListGroup.Item action onClick={this.props.onGetNextQuestion}>
+                  Next Question
+                </ListGroup.Item>
               </ListGroup>
-              <button
-                className="btn btn-secondary btn-sm"
-                onClick={this.props.onGetNextQuestion}
-              >
-                Submit
-              </button>
-            </div>
-          ) : (
-            <div>
-              <h1>Welcome to Tandem Trivia!</h1>
-              <button
-                className="btn btn-secondary btn-sm"
-                onClick={this.props.onStartGame}
-              >
-                Start Game
-              </button>
-            </div>
-          )}
-        </Jumbotron>
-      </div>
+            ) : null}
+          </div>
+        ) : (
+          <div>
+            <h1>Welcome to Tandem Trivia!</h1>
+            <button
+              className="btn btn-secondary btn-sm"
+              onClick={this.props.onStartGame}
+            >
+              Start Game
+            </button>
+          </div>
+        )}
+      </Jumbotron>
     );
   }
 }
